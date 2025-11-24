12a2925a43d8c4733c481c7d58b8114d65c11cce
Aryabell <aryabell.b@gmail.com>
2025-11-24 15:51:16 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -26,17 +26,17 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
 
     // fetch movies from the API
     private fun fetchPopularMovies() {
-    // launch a coroutine in viewModelScope
-    // Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies().catch {
-    // catch is a terminal operator that catches exceptions from the Flow
-                _error.value = "An exception occurred: ${it.message}"
-            }.collect {
-    // collect is a terminal operator that collects the values from the Flow
-    // the results are emitted to the StateFlow
-                _popularMovies.value = it
-            }
+            movieRepository.fetchMovies()
+                .catch { e ->
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movies ->
+                    val sortedMovies = movies.sortedByDescending { it.popularity }
+
+                    // update StateFlow
+                    _popularMovies.value = sortedMovies
+                }
         }
     }
 }
